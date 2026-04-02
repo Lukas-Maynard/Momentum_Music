43f6020c8416960d8ffb6bd22cfa3ff17b17d2f1
Lukas Maynard <lmaynard8@ivytech.edu>
2026-04-02 01:59:30 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Momentum_Music
 One button application for music creation
+
+Quick C# ref: https://learnxinyminutes.com/csharp/
+
+## UI framework
+Avalonia
+https://github.com/avaloniaui/avalonia
+
+### framework setup
+    https://docs.avaloniaui.net/docs/get-started/install-avalonia
+
+    `dotnet new install Avalonia.Templates`
+    `
---
+++ main.cs
@@ -0,0 +1,23 @@
+/*
+one button game
+a random instrument (from a select set) is chosen
+a | (Bar) moves across the music sheet screen, when you press the button the instrument plays at that time
+when you reach the end, the next instrument is selected, a short timer plays before starting the next loop
+You can see all the music at all times
+you cannot fix mistakes
+
+Additional:
+press once to play with metronome, twice without
+have ability to loop sections/create a larger "looping" timeline? or have it random
+have a way to save music file
+
+public leaderboard? saved to database with limited amount of saves per person
+Advanced search funciton
+*/
+
+// COMPLETE:
+
+// TODO: 
+// create window
+// make line move across the screen
+// 
